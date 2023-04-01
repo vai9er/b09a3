ed57f284723231fdd19ddff686a37683c677ae7b
Gabriel Vainer WSL <vai9er@gmail.com>
2023-04-01 03:06:48 -0400

    bruh

--- main.c
+++ main.c
@@ -109,84 +109,84 @@ void clear_screen() {
   printf("\033[%d;%dH", 0, 0);  // move cursor to the top-left corner
 }
 
-//function to print the menu
-void seqFlag(int samples, int tdelay){
-    int j = 0;
-    for (int i = 0; i < samples; i++) {
-        printf(">>> ITERATION %d\n", i+1);
-        printf("Number of samples: %d -- every %d secs\n", samples, tdelay);
+// //function to print the menu
+// void seqFlag(int samples, int tdelay){
+//     int j = 0;
+//     for (int i = 0; i < samples; i++) {
+//         printf(">>> ITERATION %d\n", i+1);
+//         printf("Number of samples: %d -- every %d secs\n", samples, tdelay);
 
-        //step 1: get system information
-        struct sysinfo systemInfo;
-        sysinfo(&systemInfo);
+//         //step 1: get system information
+//         struct sysinfo systemInfo;
+//         sysinfo(&systemInfo);
     
-        // Get total and used memory
-        float memory_total = systemInfo.totalram;
-        float memory_used = systemInfo.totalram - systemInfo.freeram;
+//         // Get total and used memory
+//         float memory_total = systemInfo.totalram;
+//         float memory_used = systemInfo.totalram - systemInfo.freeram;
 
-        // Print memory usage in kilobytes
-        printf("Memory usage: %f kilobytes\n", memory_used / 1024);
-        printf("---------------------------------------\n");
-        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
+//         // Print memory usage in kilobytes
+//         printf("Memory usage: %f kilobytes\n", memory_used / 1024);
+//         printf("---------------------------------------\n");
+//         printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
 
 
-        for (j = 0; j < i; j++){
-            printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
-        }
-        printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
-        for (j = i; j < 9; j++){
-            printf("\n");
-        }
-        printUsers();
-        logCores();
-        printMachineInfo();
-        logCpuUsage();
-        sleep(tdelay);
-        printf("\n");
-    }
-}
+//         for (j = 0; j < i; j++){
+//             printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+//         }
+//         printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+//         for (j = i; j < 9; j++){
+//             printf("\n");
+//         }
+//         printUsers();
+//         logCores();
+//         printMachineInfo();
+//         logCpuUsage();
+//         sleep(tdelay);
+//         printf("\n");
+//     }
+// }
 
-void refresh(int samples, int tdelay){
-    clear_screen();
-    int j = 0;
-    for (int i = 0; i < samples; i++) {
-        printf("Number of samples: %d -- every %d secs\n", samples, tdelay);
+// void refresh(int samples, int tdelay){
+//     clear_screen();
+//     int j = 0;
+//     for (int i = 0; i < samples; i++) {
+//         printf("Number of samples: %d -- every %d secs\n", samples, tdelay);
         
-        //step 1: get system information
-        struct sysinfo systemInfo;
-        sysinfo(&systemInfo);
+//         //step 1: get system information
+//         struct sysinfo systemInfo;
+//         sysinfo(&systemInfo);
     
-        // Get total and used memory
-        float memory_total = systemInfo.totalram;
-        float memory_used = systemInfo.totalram - systemInfo.freeram;
+//         // Get total and used memory
+//         float memory_total = systemInfo.totalram;
+//         float memory_used = systemInfo.totalram - systemInfo.freeram;
 
-        // Print memory usage in kilobytes
-        printf("Memory usage: %.0f kilobytes\n", (memory_used / 1024)/1024);
-        printf("---------------------------------------\n");
-        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
+//         // Print memory usage in kilobytes
+//         printf("Memory usage: %.0f kilobytes\n", (memory_used / 1024)/1024);
+//         printf("---------------------------------------\n");
+//         printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
 
-        // Print memory usage in kilobytes before
-        for (j = 0; j < i; j++){
-            printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
-        }
-        // Print current memory usage in kilobytes
-        printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+//         // Print memory usage in kilobytes before
+//         for (j = 0; j < i; j++){
+//             printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+//         }
+//         // Print current memory usage in kilobytes
+//         printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", memory_used / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
 
-        // Print memory usage in kilobytes after
-        for (j = i; j < 9; j++){
-            printf("\n");
-        }
-        //print the rest of the information
-        printUsers();
-        logCores();
-        printMachineInfo();
-        logCpuUsage();
-        clear_screen();
+//         // Print memory usage in kilobytes after
+//         for (j = i; j < 9; j++){
+//             printf("\n");
+//         }
+//         //print the rest of the information
+//         printUsers();
+//         logCores();
+//         printMachineInfo();
+//         logCpuUsage();
+//         clear_screen();
         
-        //gn
-        sleep(tdelay);
-    }
-}
+//         //gn
+//         sleep(tdelay);
+//     }
+// }
 
 int main(int argc, char** argv){
     //default values
@@ -201,22 +201,22 @@ int main(int argc, char** argv){
 
     //--system
     if (systemm == 1) {
-        printMemUsage(samples, tdelay);
-        logCores();
-        logCpuUsage();
-        printMachineInfo();
+        // printMemUsage(samples, tdelay);
+        // logCores();
+        // logCpuUsage();
+        // printMachineInfo();
     } 
 
     //--user
     else if (user == 1) {
-        printUsers();
+        // printUsers();
     } 
 
     //--sequential
     else if (sequential == 1) {
 
         //warnng: give it some time if you are trying to output to a file
-        seqFlag(samples,tdelay);
+        // seqFlag(samples,tdelay);
 
     } 
     else {
