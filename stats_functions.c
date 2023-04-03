8526326709c95b68e6bee82b2020dfc3b8498b97
Gabriel Vainer WSL <vai9er@gmail.com>
2023-04-03 05:12:53 -0400

    hmm

--- stats_functions.c
+++ stats_functions.c
@@ -440,7 +440,7 @@ void graphicalRefresh(int samples, int tdelay){
 
             printMachineInfo();
         }
-        free(mem_info.memory_used);
+
         close(machine_pipe[READ_END]);
         close(memory_pipe[READ_END]);
         close(cpu_pipe[READ_END]);
@@ -448,6 +448,130 @@ void graphicalRefresh(int samples, int tdelay){
 }
 
 void defaultOutput(int samples, int tdelay){
+    int machine_pipe[2];
+    pid_t machine_pid;
+    int memory_pipe[2];
+    pid_t memory_pid;
+    int cpu_pipe[2];
+    pid_t cpu_pid;
+
+    if (pipe(machine_pipe) == -1 || pipe(memory_pipe) == -1 || pipe(cpu_pipe) == -1) {
+        fprintf(stderr,"Pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    machine_pid = fork();
+    if (machine_pid < 0) {
+        fprintf(stderr,"Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (machine_pid == 0) {
+        logSessional(machine_pipe, samples, tdelay);
+        exit(EXIT_SUCCESS);
+    }
+
+    memory_pid = fork();
+    if (memory_pid < 0) {
+        fprintf(stderr,"Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (memory_pid == 0) {
+        // Memory child process
+        close(memory_pipe[READ_END]);
+        // Perform memory utilization task and write results to pipe
+        getMemUtil(samples, tdelay, memory_pipe);
+        close(memory_pipe[WRITE_END]);
+        exit(EXIT_SUCCESS);
+    }
+
+    cpu_pid = fork();
+    if (cpu_pid < 0) {
+        fprintf(stderr,"Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (cpu_pid == 0) {
+        // CPU child process
+        close(cpu_pipe[READ_END]);
+        // Perform CPU utilization task and write results to pipe
+        getCpuUsage(samples, tdelay, cpu_pipe);
+        close(cpu_pipe[WRITE_END]);
+        exit(EXIT_SUCCESS);
+    } else {
+        // Parent process
+        close(memory_pipe[WRITE_END]);
+
+        struct mem_info mem_info;
+        read(memory_pipe[READ_END], &mem_info, sizeof(mem_info));
+        mem_info.memory_used = malloc(mem_info.num_samples * sizeof(float));
+        //read(memory_pipe[READ_END], mem_info.memory_used, mem_info.num_samples * sizeof(float));
+
+        struct session_info info;
+        close(machine_pipe[WRITE_END]);
+        
+        struct cpu_info cpu_info[samples];
+
+        
+        float memory_usage[samples];
+        for (int i = 0; i < mem_info.num_samples; i++) {
+            read(cpu_pipe[READ_END], &cpu_info[i], sizeof(cpu_info[i]));
+            clear_screen();
+            memory_usage[i] = mem_info.memory_used[i];
+
+            printf("Nbr of samples: %d -- every %d secs\n", samples, tdelay);
+            struct sysinfo systemInfo;
+            sysinfo(&systemInfo);
+
+            float memory_total = systemInfo.totalram;
+            float memory_used = systemInfo.totalram - systemInfo.freeram;
+
+              
+            printf("Memory usage: %.0f kilobytes\n", (memory_used / 1024)/1024);
+            printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
+            printf("---------------------------------------\n");
+
+            struct memory_display memory_display[samples];
+            for (int j = 0; j < i; j++) {
+                float change = memory_usage[j+1] - memory_usage[j];
+                printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB\n", mem_info.memory_used[j] / (1024 * 1024 * 1024), mem_info.memory_total / (1024 * 1024 * 1024), mem_info.memory_used[j] / (1024 * 1024 * 1024), (mem_info.memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+            }
+            printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB", mem_info.memory_used[i] / (1024 * 1024 * 1024), mem_info.memory_total / (1024 * 1024 * 1024), mem_info.memory_used[i] / (1024 * 1024 * 1024), (mem_info.memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+
+            
+            read(machine_pipe[READ_END], &info, sizeof(info));
+
+            for(int z = 0; z < samples - i; z++){
+                printf("\n");
+            }
+            printf("---------------------------------------\n");
+            printf("### Sessions/users ### \n");
+            for (int j = 0; j < info.num_users; j++) {
+                printf("%s", info.users[j]);
+            }
+
+            printf("---------------------------------------\n");
+            printf("Number of cores: %d \n", get_nprocs());
+            printf("total cpu use = %.2f%%\n", cpu_info[i].utilization);
+
+            // for (int k = 0; k <= i; k++) {
+            //     if (cpu_info[k].num_bars > 0) {
+            //         printf("\t");
+            //         for (int j = 0; j < cpu_info[k].num_bars; j++) {
+            //             printf("|");
+            //         }
+            //         printf(" %.2f\n", cpu_info[k].utilization);
+            //     } else {
+            //         printf("\t %.2f\n", cpu_info[k].utilization);
+            //     }
+            // }
+
+            printMachineInfo();
+
+        }
+        free(mem_info.memory_used);
+        close(machine_pipe[READ_END]);
+        close(memory_pipe[READ_END]);
+        close(cpu_pipe[READ_END]);
+    }
+}
+
+void defaultOutput2(int samples, int tdelay){
     int machine_pipe[2];
     pid_t machine_pid;
     int memory_pipe[2];
